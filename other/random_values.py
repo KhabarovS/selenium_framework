18e4e0113e3a34d7d80630831b9207bd73a89316
KhabarovS <delkuenta@gmail.com>
2024-01-20 09:31:29 +0300

    [+] Добавлены генераторы работы, имени, почты

--- other/random_values.py
+++ other/random_values.py
@@ -1,14 +1,23 @@
-from datetime import date, datetime, timedelta
+from datetime import datetime
+from enum import Enum
 from random import randint
 from string import ascii_lowercase, digits
 from typing import Optional
 
 from allure import step
+from mimesis import Person, Locale
 from mimesis.random import Random
 
 from other.logging import logger
 
 msg = 'ГЕНЕРАТОР {gen}.\nРЕЗУЛЬТАТ: {result}'
+
+
+class NameEnum(Enum):
+    """ Перечисление возможных имен"""
+    FULL_NAME = 'full_name'
+    LAST_NAME = 'last_name'
+    FIRST_NAME = 'first_name'
 
 
 @step('Генератор. Получить случайное число')
@@ -27,7 +36,7 @@ def get_random_number(start: int = 1, end: int = 999999, length: Optional[int] =
 
 
 @step('Генератор. Получить случайную строку')
-def get_random_string(length: int):
+def get_random_string(length: int = 10):
     """ Получить случайную строку
 
     Args:
@@ -38,5 +47,40 @@ def get_random_string(length: int):
         length=length
     )
     logger.info(msg.format(gen='cлучайной строки', result=result))
+
+    return result
+
+
+def get_random_name(name: NameEnum = NameEnum.FULL_NAME) -> str:
+    """Получить случайное имя
+
+    Args:
+        name: тип имени(полное имя, имя или фамилия)
+    """
+    result = getattr(Person(Locale.EN), name.value)()
+
+    logger.info(msg.format(gen='случайного имени', result=result))
+
+    return result
+
+
+def get_random_job() -> str:
+    """ Получить случайную работу"""
+    result = Person(Locale.EN).occupation()
+
+    logger.info(msg.format(gen='случайной работы', result=result))
+
+    return result
+
+
+def get_random_email(domains: tuple[str] = ('mail.ru', 'gmail.com', 'yandex.ru')) -> str:
+    """ Получить случайный e-mail
+
+    Args:
+        domains: набор доменов
+    """
+    result = Person(locale=Locale.RU).email(domains=domains)
+
+    logger.info(msg.format(gen='случайной почты', result=result))
 
     return result
